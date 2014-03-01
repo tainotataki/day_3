ee1150930f5eb744ae5377036960ddc8d59418b1
Kota Uno <kuno@wesleyan.edu>
2014-03-01 11:50:19 -0500

    all

---
+++ spacecat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spacecat::Application.config.secret_key_base = '71f373a90e0695076c46dab25475f4b10d711abad2f3c427f487f4ef7db5946234c1e52e2c7fdf288d741b8f895c49a1b6dd9bd3162cfa5e3e9874e7ab659be0'
---
+++ spacecat/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spacecat::Application.config.session_store :cookie_store, key: '_spacecat_session'
